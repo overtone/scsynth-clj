0c72ab00e88c227b6da51144c555d64ad123f3b5
Fabian Aussems <fabianaussems@gmail.com>
2011-04-26 14:18:26 +0200

    Added clj-native and scsynth-clj deps as dependency

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Supercollider Interop for Clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.bagucode/jna "3.2.4"]
+                 [overtone/clj-native "0.9.2-SNAPSHOT"]
+                 [overtone/scsynth-clj-native-deps "0.0.1-SNAPSHOT"]
                  ;[overtone/osc-clj "0.3.0-SNAPSHOT"]
                  ])
